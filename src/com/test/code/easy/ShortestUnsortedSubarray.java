0d7db94084d6c565eb3f6f491f9d49f52a01eb12
kattavi <vijayakumar.katta@verizon.com>
2019-12-02 15:59:30 -0500

    shortest subarray

---
+++ src/com/test/code/easy/ShortestUnsortedSubarray.java
@@ -0,0 +1,33 @@
+package com.test.code.easy;
+
+import java.util.Arrays;
+
+// 581 leetcode
+
+public class ShortestUnsortedSubarray {
+
+	public static void main(String[] args) {
+		
+		int[] input = {2, 6, 4, 8, 10, 9, 15};
+		
+		System.out.println(getSubArray(input));
+		
+	}
+
+	private static int getSubArray(int[] input) {
+		int[] sinput = input.clone();
+		Arrays.sort(sinput);
+		int start = sinput.length,end = 0;
+		for(int i=0;i<sinput.length;i++)
+		{
+			if(input[i]!=sinput[i])
+			{
+				start = Math.min(start, i);
+				end = Math.max(end, i);
+			}
+		}
+		
+		return (end-start >= 0?end-start+1:0);
+	}
+
+}
