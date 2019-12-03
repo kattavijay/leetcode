f29d871604adf0119fbc426b963382667434da4d
kattavijay <vijay@Vijayas-MBP.home>
2019-12-02 22:35:34 -0500

    formatted the code

--- src/com/test/code/easy/ArrayToInteger.java
+++ src/com/test/code/easy/ArrayToInteger.java
@@ -5,29 +5,26 @@ import java.util.List;
 
 public class ArrayToInteger {
 
-	public List<Integer> getvalue(int[] a, int k)
-	{
+	public List<Integer> getvalue(int[] a, int k) {
 		long temp = 0;
-		String tValue ="";
-		
-		for(int i=0;i<a.length;i++)
-		{
-			tValue = tValue+Integer.toString(a[i]);
+		String tValue = "";
+
+		for (int i = 0; i < a.length; i++) {
+			tValue = tValue + Integer.toString(a[i]);
 		}
-		
+
 		System.out.println(tValue);
 		temp = Long.parseLong(tValue) + k;
 		List<Integer> l = new ArrayList<Integer>();
-		
+
 		String value = Long.toString(temp);
-		
+
 		System.out.println(value);
-		
-		for(int j=0;j<value.length();j++)
-		{
+
+		for (int j = 0; j < value.length(); j++) {
 			l.add(Character.getNumericValue(value.charAt(j)));
 		}
-		
+
 		return l;
 	}
 
--- src/com/test/code/easy/ArrayToIntegerTest.java
+++ src/com/test/code/easy/ArrayToIntegerTest.java
@@ -11,21 +11,22 @@ public class ArrayToIntegerTest {
 
 	@Test
 	public void test() {
-		
+
 		ArrayToInteger ai = new ArrayToInteger();
-		int[] a = {1,2,3};
+		int[] a = { 1, 2, 3 };
 		List<Integer> l = new ArrayList<Integer>();
 		l.add(1);
 		l.add(3);
 		l.add(3);
-		
+
 		assertEquals(l, ai.getvalue(a, 10));
 	}
+
 	@Test
 	public void test1() {
-		
+
 		ArrayToInteger ai = new ArrayToInteger();
-		int[] a = {9,9,9,9,9,9,9,9,9};
+		int[] a = { 9, 9, 9, 9, 9, 9, 9, 9, 9 };
 		List<Integer> l = new ArrayList<Integer>();
 		l.add(1);
 		l.add(0);
@@ -37,17 +38,15 @@ public class ArrayToIntegerTest {
 		l.add(0);
 		l.add(0);
 		l.add(0);
-	
-		
-		
+
 		assertEquals(l, ai.getvalue(a, 1));
 	}
-	
+
 	@Test
 	public void test2() {
-		
+
 		ArrayToInteger ai = new ArrayToInteger();
-		int[] a = {9,9,9,9,9,9,9,9,9,9};
+		int[] a = { 9, 9, 9, 9, 9, 9, 9, 9, 9, 9 };
 		List<Integer> l = new ArrayList<Integer>();
 		l.add(1);
 		l.add(0);
@@ -60,9 +59,7 @@ public class ArrayToIntegerTest {
 		l.add(0);
 		l.add(0);
 		l.add(0);
-	
-		
-		
+
 		assertEquals(l, ai.getvalue(a, 1));
 	}
 }
