abd1d23479ad176e42846e1d0d52c80c2308ffe6
kattavijay <katta.vijayk@gmail.com>
2019-12-04 07:29:29 -0500

    status update

---
+++ src/com/test/code/easy/ReverseVowels.java
@@ -0,0 +1,22 @@
+package com.test.code.easy;
+
+public class ReverseVowels {
+	
+	
+	  public String reverseVowels(String s) {
+		  
+		 String result ="";
+		 char[] temp = s.toCharArray();
+		 
+		 int start=0,end=s.length()-1;
+		 
+		 while(start<end) {
+			 
+			
+			 			 
+		 }
+			 
+		return result;  
+	    }
+
+}
