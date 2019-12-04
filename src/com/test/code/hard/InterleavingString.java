7a629da5a4c1045044be0981a4623178c1faed51
kattavijay <katta.vijayk@gmail.com>
2019-12-04 15:23:51 -0500

    updating workspace

---
+++ src/com/test/code/hard/InterleavingString.java
@@ -0,0 +1,12 @@
+package com.test.code.hard;
+
+public class InterleavingString {
+	public boolean isInterleave(String s1, String s2, String s3) {
+		
+		
+		
+		return false;
+
+	}
+
+}
---
+++ src/com/test/code/hard/WordLadder2.java
@@ -0,0 +1,20 @@
+package com.test.code.hard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordLadder2 {
+public List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+     List<String> node = new ArrayList<>();
+     List<List<String>> data = new ArrayList<>();
+     
+     if(!wordList.contains(beginWord)||!wordList.contains(beginWord))
+    	 return data;
+     
+     
+	
+	
+     return data;   
+    }
+
+}
